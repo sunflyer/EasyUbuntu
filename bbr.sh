e3f2b90a7aa768fa12125133757a866c754c7330
Wilson Chen <sunflyer@users.noreply.github.com>
2016-12-27 23:50:46 +0800

    Create bbr.sh

---
+++ bbr.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+wget http://kernel.ubuntu.com/~kernel-ppa/mainline/v4.9/linux-image-4.9.0-040900-generic_4.9.0-040900.201612111631_amd64.deb -O linux-4.9.deb
+dpkg -i linux-4.9.deb
+rm linux-4.9.deb
+echo "net.core.default_qdisc=fq" >> /etc/sysctl.conf
+echo "net.ipv4.tcp_congestion_control=bbr" >> /etc/sysctl.conf
+sysctl -p
+reboot
