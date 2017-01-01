9bfe8bef4be484d3b9adacda35cc48aa8b5a6997
Wilson Chen <sunflyer@users.noreply.github.com>
2017-01-02 01:58:05 +0800

    Create openvpn-privnet.sh
    多服务器的OpenVPN内网组建工具，for ubuntu 14/16

---
+++ openvpn-privnet.sh
@@ -0,0 +1,115 @@
+#!/bin/bash
+echo "This is an OpenVPN Private Network Setting Tool"
+echo "By CrazyChen @ Jan 2,2016"
+
+VER="1.0.0.1"
+DATE="20170102"
+
+helpinf(){
+        echo -e "Supported Usage : \nsetup.sh [OPERATION]\nWhere OPERATION coule be:\n"
+         echo -e "\tinst : install server\n\tclt : generate client cert and conf\n\tversion : show version of this tools\n\tupdate : update this script"
+}
+
+genkey(){
+        echo "Generating DH Param"
+        openssl dhparam -out /etc/openvpn/dh2048.pem 2048
+        echo "Operating"
+        cd /etc/openvpn/easy-rsa
+        . ./vars
+        echo "Cleaning all"
+        ./clean-all
+        echo "Building ca cert"
+        ./build-ca
+        echo "Building Server cert"
+        ./build-key-server server
+        echo "Copy keys to openvpn dir"
+        cp /etc/openvpn/easy-rsa/keys/{server.crt,server.key,ca.crt} /etc/openvpn
+}
+
+svrconf(){
+        echo "Please tell me which port you would like to use ? "
+        read PORT
+        if [ $PORT -lt '0' ]; then
+            PORT=1194
+        fi
+        echo "Please tell me which proto you would like to use ? (t for TCP and u for UDP , by default UDP is used) "
+        PROTOCOL="udp"
+        read PROTOCOL
+        if [ $PROTOCOL == 't']; then
+            PROTOCOL='tcp'
+        fi
+        #write conf file
+cat>/etc/openvpn/server.conf<<EOF
+        port $PORT
+        proto $PROTOCOL
+        dev tun
+        ca ca.crt
+        cert server.crt
+        key server.key 
+        dh dh2048.pem
+        server 192.168.240.0 255.255.244.0
+        ifconfig-pool-persist ipp.txt
+        client-to-client
+        keepalive 10 120
+        cipher AES-128-CBC
+        comp-lzo
+        user nobody
+        group nogroup
+        persist-key
+        persist-tun
+        status openvpn-status.log
+        log         /var/log/openvpn.log
+        verb 3
+EOF
+echo "Gen conf complete"
+}
+
+gencltConf(){
+        echo "Config name for client is $1"
+}
+
+genclt(){
+        cd /etc/openvpn/easy-rsa
+        echo "Client Cert : input a name for your client "
+        read NAME
+        ./build-key $NAME
+        gencltConf $NAME
+}
+
+install(){
+        echo "Install instruction detected , go installation"
+        apt-get update && apt-get upgrade -y && apt-get install openvpn easy-rsa  -y --force-yes
+        echo "Generating RSA Key , just press ENTER if asked by system"       
+        cp -r /usr/share/easy-rsa/ /etc/openvpn
+        mkdir /etc/openvpn/easy-rsa/keys
+        genkey   
+        echo "Configuring svr"
+        svrconf
+        echo "Config complete".
+}
+
+if [ $# -eq '0' ]; then
+        echo -e "Param Invalid !\n\n"
+        helpinf
+        exit
+fi
+
+FUNC=$1
+
+case $FUNC in
+        inst)
+                install
+                ;;
+        clt)
+
+                ;;
+        version)
+
+                ;;
+        update)
+
+                ;;
+        *)
+                helpinf
+                ;;
+esac
