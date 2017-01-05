0a74aa6515288ab422029e1f7ecb03db1ba009a1
Wilson Chen <sunflyer@users.noreply.github.com>
2017-01-05 23:12:15 +0800

    Laravel install src

---
+++ laravel.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+wget https://getcomposer.org/installer -O installer
+php installer
+mv composer.phar /usr/local/bin/composer
+
+echo "Making User , please input user name for laravel : "
+read NAME
+useradd $NAME -m -d /home/$NAME -s /bin/bash
+echo "Please set a password for user $NAME"
+passwd $NAME
+echo "Changing to $NAME"
+su $NAME
+cd ~/
+composer global require "laravel/installer"
+echo "Completed."
+
