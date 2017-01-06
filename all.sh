ef8f26f2c84dea5251a10cbb35aa43a6a8529007
Wilson Chen <sunflyer@users.noreply.github.com>
2017-01-06 15:25:05 +0800

    get src

--- all.sh
+++ all.sh
@@ -1,16 +1,29 @@
 #!/bin/bash
 
 BASE_ADDR="https://github.com/sunflyer/EasyUbuntu/raw/master/"
+
+getsrc(){
+    echo "Fetching $1"
+    URL="$BASE_ADDR/$1"
+    wget $URL -O $1
+    chmod +x $1
+    echo "Executing $1"
+    ./$1
+}
+
+
+if [ $# -gt '0' ]; then
+    for src in $@ do    
+        getsrc $src
+    done
+    exit
+fi
+
 SRC=('lnmp.sh' 'setHttps.sh' 'shadowsock.sh' 'jdkenv1.8.sh' 'bbr.sh')
 
 echo "Checking Wget "
 apt-get update && apt-get install wget
 
 for src in ${SRC[@]}; do
-    echo "Fetching $src"
-    URL="$BASE_ADDR/$src"
-    wget $ULR -O $src
-    chmod +x $src
-    echo "Executing $src"
-    ./$src
+    getsrc $src
 done
