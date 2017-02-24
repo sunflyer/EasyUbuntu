ae7deaf8788dec0423ca8628f70793b12051aaf6
Wilson Chen <sunflyer@users.noreply.github.com>
2017-02-24 10:58:18 +0800

    Update bbr.sh

--- bbr.sh
+++ bbr.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-wget http://kernel.ubuntu.com/~kernel-ppa/mainline/v4.9/linux-image-4.9.0-040900-generic_4.9.0-040900.201612111631_amd64.deb -O linux-4.9.deb
+#ADDR=http://kernel.ubuntu.com/~kernel-ppa/mainline/v4.9/linux-image-4.9.0-040900-generic_4.9.0-040900.201612111631_amd64.deb
+ADDR=http://kernel.ubuntu.com/~kernel-ppa/mainline/v4.9.12/linux-image-4.9.12-040912-generic_4.9.12-040912.201702231232_amd64.deb
+wget $ADDR -O linux-4.9.deb
 dpkg -i linux-4.9.deb
 rm linux-4.9.deb
 echo "net.core.default_qdisc=fq" >> /etc/sysctl.conf
