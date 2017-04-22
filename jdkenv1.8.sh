6149c93dcc556e20314f7c6b6b5ca37fbedde403
Wilson Chen <sunflyer@users.noreply.github.com>
2017-04-22 22:49:15 +0800

    Update jdkenv1.8.sh

--- jdkenv1.8.sh
+++ jdkenv1.8.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+curl -H "Cookie: s_cc=true; oraclelicense=accept-securebackup-cookie; s_nr=1492872107355; gpw_e24=http%3A%2F%2Fwww.oracle.com%2Ftechnetwork%2Fjava%2Fjavase%2Fdownloads%2Fjdk8-downloads-2133151.html; s_sq=%5B%5BB%5D%5D" -H "User-Agent: Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.133 Safari/537.36" -O -L http://download.oracle.com/otn-pub/java/jdk/8u131-b11/d54c1d3a095b4ff2b6607d096fa80163/jdk-8u131-linux-x64.tar.gz
+tar zxf jdk-8u131-linux-x64.tar.gz
+mkdir /usr/lib/jvm
+mv jdk1.8.0* /usr/lib/jvm/jdk1.8.0
 cat >> /etc/profile << EOF
 export JAVA_HOME=/usr/lib/jvm/jdk1.8.0
 export CLASSPATH=.:\$JAVA_HOME/lib:\$JAVA_HOME/jre/lib:\$CLASSPATH
