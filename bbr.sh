0c4913395ecbbe11591dc9c690d2d52ad91f3170
Wilson Chen <sunflyer@users.noreply.github.com>
2017-06-16 21:04:17 +0800

    Update bbr.sh
    update 4.9.32 for CVE2017-8890

--- bbr.sh
+++ bbr.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 #ADDR=http://kernel.ubuntu.com/~kernel-ppa/mainline/v4.9/linux-image-4.9.0-040900-generic_4.9.0-040900.201612111631_amd64.deb
 #ADDR=http://kernel.ubuntu.com/~kernel-ppa/mainline/v4.9.12/linux-image-4.9.12-040912-generic_4.9.12-040912.201702231232_amd64.deb
-ADDR=http://kernel.ubuntu.com/~kernel-ppa/mainline/v4.9.25/linux-image-4.9.25-040925-generic_4.9.25-040925.201705041424_amd64.deb
-wget $ADDR -O linux-4.9.deb
+#ADDR=http://kernel.ubuntu.com/~kernel-ppa/mainline/v4.9.25/linux-image-4.9.25-040925-generic_4.9.25-040925.201705041424_amd64.deb
+ADDR=http://kernel.ubuntu.com/~kernel-ppa/mainline/v4.9.32/linux-image-4.9.32-040932-generic_4.9.32-040932.201706141032_amd64.deb
+ADDR_HEADER=http://kernel.ubuntu.com/~kernel-ppa/mainline/v4.9.32/linux-headers-4.9.32-040932-generic_4.9.32-040932.201706141032_amd64.deb
+wget "${ADDR}" -O linux-4.9.deb
+wget "${ADDR_HEADER}" -O linux-header-4.9.deb
 dpkg -i linux-4.9.deb
+dpkg -i linux-header-4.9.deb
 rm linux-4.9.deb
+rm linux-header-4.9.deb
 echo "net.core.default_qdisc=fq" >> /etc/sysctl.conf
 echo "net.ipv4.tcp_congestion_control=bbr" >> /etc/sysctl.conf
 sysctl -p
