db60cf7c3657fbd23f7d49d00452512e8b6b7709
Wilson Chen <sunflyer@users.noreply.github.com>
2017-06-16 21:08:20 +0800

    Update bbr.sh

--- bbr.sh
+++ bbr.sh
@@ -4,10 +4,13 @@
 #ADDR=http://kernel.ubuntu.com/~kernel-ppa/mainline/v4.9.25/linux-image-4.9.25-040925-generic_4.9.25-040925.201705041424_amd64.deb
 ADDR=http://kernel.ubuntu.com/~kernel-ppa/mainline/v4.9.32/linux-image-4.9.32-040932-generic_4.9.32-040932.201706141032_amd64.deb
 ADDR_HEADER=http://kernel.ubuntu.com/~kernel-ppa/mainline/v4.9.32/linux-headers-4.9.32-040932-generic_4.9.32-040932.201706141032_amd64.deb
+ADDR_HEADER_H=http://kernel.ubuntu.com/~kernel-ppa/mainline/v4.9.32/linux-headers-4.9.32-040932_4.9.32-040932.201706141032_all.deb
 wget "${ADDR}" -O linux-4.9.deb
 wget "${ADDR_HEADER}" -O linux-header-4.9.deb
+wget "${ADDR_HEADER_H}" -O linux-header-4.9-h.deb
 dpkg -i linux-4.9.deb
 dpkg -i linux-header-4.9.deb
+dpkg -i linux-header-4.9-h.deb
 rm linux-4.9.deb
 rm linux-header-4.9.deb
 echo "net.core.default_qdisc=fq" >> /etc/sysctl.conf
