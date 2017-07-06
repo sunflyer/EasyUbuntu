1b1db3b319192f2e66716418663d8fa148476885
Wilson Chen <sunflyer@users.noreply.github.com>
2017-07-06 22:06:41 +0800

    Update bbr.sh

--- bbr.sh
+++ bbr.sh
@@ -7,17 +7,17 @@ if [ $# -gt '0' ]; then
 fi
 
 ADDR=http://kernel.ubuntu.com/~kernel-ppa/mainline/v4.9.36/linux-image-4.9.36-040936-generic_4.9.36-040936.201707050932_amd64.deb
-ADDR_HEADER=http://kernel.ubuntu.com/~kernel-ppa/mainline/v4.9.36/linux-headers-4.9.36-040936-generic_4.9.36-040936.201707050932_amd64.deb
-ADDR_HEADER_H=http://kernel.ubuntu.com/~kernel-ppa/mainline/v4.9.36/linux-headers-4.9.36-040936_4.9.36-040936.201707050932_all.deb
+#ADDR_HEADER=http://kernel.ubuntu.com/~kernel-ppa/mainline/v4.9.36/linux-headers-4.9.36-040936-generic_4.9.36-040936.201707050932_amd64.deb
+#ADDR_HEADER_H=http://kernel.ubuntu.com/~kernel-ppa/mainline/v4.9.36/linux-headers-4.9.36-040936_4.9.36-040936.201707050932_all.deb
 wget "${ADDR}" -O linux-4.9.deb
-wget "${ADDR_HEADER_H}" -O linux-header-4.9-h.deb
-wget "${ADDR_HEADER}" -O linux-header-4.9.deb
+#wget "${ADDR_HEADER_H}" -O linux-header-4.9-h.deb
+#wget "${ADDR_HEADER}" -O linux-header-4.9.deb
 dpkg -i linux-4.9.deb
-dpkg -i linux-header-4.9-h.deb
-dpkg -i linux-header-4.9.deb
+#dpkg -i linux-header-4.9-h.deb
+#dpkg -i linux-header-4.9.deb
 rm linux-4.9.deb
-rm linux-header-4.9.deb
-rm linux-header-4.9-h.deb
+#rm linux-header-4.9.deb
+#rm linux-header-4.9-h.deb
 
 if [ ${UPDATE} -eq '1' ]; then
     echo "net.core.default_qdisc=fq" >> /etc/sysctl.conf
