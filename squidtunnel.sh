ce062c51842f1011ea810b3630d863f6801482bf
Wilson Chen <sunflyer@users.noreply.github.com>
2017-10-18 21:18:03 +0800

    Update squidtunnel.sh

--- squidtunnel.sh
+++ squidtunnel.sh
@@ -9,12 +9,13 @@ PUBKEY=$1
 PRIVKEY=$2
 
 apt-get update && apt-get upgrade -y
-apt-get install squid3 stunnel apache2-utils -y && apt-get clean
-mkdir /etc/squid3
-cat > /etc/squid3/squid.conf << EOF
+apt-get install squid stunnel apache2-utils -y && apt-get clean
+#mkdir /etc/squid
+mv /etc/squid/squid.conf /etc/squid/squid.conf.bak
+cat > /etc/squid/squid.conf << EOF
 http_port 127.0.0.1:18080
 acl CONNECT method CONNECT
-auth_param basic program /usr/lib/squid3/basic_ncsa_auth /etc/squid3/squid.passwd
+auth_param basic program /usr/lib/squid/basic_ncsa_auth /etc/squid/squid.passwd
 auth_param basic children 5
 auth_param basic realm Non-Authoritive Service Password
 auth_param basic credentialsttl 2 hours
@@ -29,7 +30,7 @@ echo "Input user name for Proxy Authentication :"
 read NAME
 echo "Input password for Proxy Authentication : "
 read PASSWORD
-htpasswd -bc /etc/squid3/squid.passwd $NAME $PASSWORD
+htpasswd -bc /etc/squid/squid.passwd $NAME $PASSWORD
 echo "Now configuring Stunnel"
 mkdir /etc/stunnel
 cat > /etc/stunnel/stunnel.conf << EOF
@@ -52,7 +53,7 @@ EOF
 echo "Checking config of Squid"
 squid3 -k check
 echo "restarting service"
-service squid3 restart
+service squid restart
 service stunnel4 restart
 
 echo "Service Deploy complete , please use "
