a1db8b57b13ab6fe3a0d08f883be610824414f7c
Wilson Chen <sunflyer@users.noreply.github.com>
2017-12-02 13:42:32 +0800

    Create newSysPrepare.sh

---
+++ newSysPrepare.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#懒的一个一个下载了，直接拉一群自动执行
+
+SCR_LIST="jdkenv1.8.sh,lnmp.sh,mkswap.sh,setHttps.sh,v2ray.sh"
+DOWNADDR="https://github.com/sunflyer/EasyUbuntu/raw/master/"
+
+OLD_IFS="${IFS}"
+IFS=","
+
+LIST=(${SCR_LIST})
+LIST_E=(${EXEC_LIST})
+
+IFS=${OLD_IFS}
+
+for x in ${LIST[@]}
+do
+        ADDR="${DOWNADDR}${x}"
+        echo "Getting script [${x}] from [${ADDR}]"
+        curl -OL ${ADDR}
+        chmod +x ${x}
+        bash {x}
+done
