430d6b98596d294f9d6c4c40143931ff0dfe162c
Wilson Chen <sunflyer@users.noreply.github.com>
2017-12-02 13:29:54 +0800

    Update lnmp.sh

--- lnmp.sh
+++ lnmp.sh
@@ -75,7 +75,7 @@ fi
 mv /etc/nginx/nginx.conf /etc/nginx/nginx.conf.bak
 cat >> /etc/nginx/nginx.conf << EOF
 user  nginx;
-worker_processes  1;
+worker_processes  auto;
 
 error_log  /var/log/nginx/error.log warn;
 pid        /var/run/nginx.pid;
