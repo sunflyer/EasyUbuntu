7cdc7eb92737db7a199d32906ea4335dd6100ee1
Wilson Chen <sunflyer@users.noreply.github.com>
2018-02-05 16:27:21 +0800

    Update removeAliyunDaemon.sh

--- removeAliyunDaemon.sh
+++ removeAliyunDaemon.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
+#
+# 用过阿里云的都知道这脚本是想拿来干嘛的
+# 
 curl -sSL http://update.aegis.aliyun.com/download/quartz_uninstall.sh | sudo bash
 sudo rm -rf /usr/local/aegis
-sudo rm /usr/sbin/aliyun-service
+sudo rm /usr/sbin/aliyun-service*
 sudo rm /lib/systemd/system/aliyun.service
+
