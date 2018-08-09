0dc03809bad62e345169806d587c23dce2dca8cb
Wilson Chen <sunflyer@users.noreply.github.com>
2018-08-09 18:03:17 +0800

    Update bbr.sh

--- bbr.sh
+++ bbr.sh
@@ -7,7 +7,8 @@ if [ $# -gt '0' ]; then
 fi
 
 #ADDR="http://kernel.ubuntu.com/~kernel-ppa/mainline/v4.9.75/linux-image-4.9.75-040975-generic_4.9.75-040975.201801051530_amd64.deb"
-ADDR="http://kernel.ubuntu.com/~kernel-ppa/mainline/v4.9.87/linux-image-4.9.87-040987-generic_4.9.87-040987.201803111631_amd64.deb"
+#ADDR="http://kernel.ubuntu.com/~kernel-ppa/mainline/v4.9.87/linux-image-4.9.87-040987-generic_4.9.87-040987.201803111631_amd64.deb"
+ADDR="http://kernel.ubuntu.com/~kernel-ppa/mainline/v4.9.118/linux-image-4.9.118-0409118-generic_4.9.118-0409118.201808061531_amd64.deb"
 wget "${ADDR}" -O linux-4.9.deb
 #wget "${ADDR_HEADER_H}" -O linux-header-4.9-h.deb
 #wget "${ADDR_HEADER}" -O linux-header-4.9.deb
