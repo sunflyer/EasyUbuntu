f8849ba1fda282bc068956d0bc3b01b515316092
Wilson Chen <sunflyer@users.noreply.github.com>
2018-10-18 22:08:27 +0800

    Update squidtunnel.sh

--- squidtunnel.sh
+++ squidtunnel.sh
@@ -21,6 +21,17 @@ auth_param basic realm Non-Authoritive Service Password
 auth_param basic credentialsttl 2 hours
 auth_param basic casesensitive off
 acl ncsa_users proxy_auth REQUIRED
+
+acl internal dst 172.16.0.0/12
+acl internal dst 10.0.0.0/8
+acl internal dst 100.64.0.0/10
+acl internal dst 192.168.0.0/16
+acl dstlocal dst 127.0.0.0/8
+acl dstlocal dst ::1
+
+http_access deny internal
+http_access deny dstlocal
+
 http_access allow ncsa_users
 http_access deny all
 httpd_suppress_version_string on
@@ -30,6 +41,11 @@ forwarded_for off
 follow_x_forwarded_for deny all
 request_header_access X-Forwarded-For deny all
 header_access X_Forwarded_For deny all
+error_directory /usr/share/squid/errors/en
+reply_header_access X-Squid-Error deny all
+reply_header_access Server deny all
+reply_header_access X-Cache deny all
+reply_header_access X-Cache-Lookup deny all
 EOF
 
 echo "Input user name for Proxy Authentication :"
