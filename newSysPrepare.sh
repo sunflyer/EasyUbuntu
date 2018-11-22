0142923c86f6a2e832ed9f005ff41ac6634dcfda
Wilson Chen <sunflyer@users.noreply.github.com>
2018-11-22 18:44:20 +0800

    Update newSysPrepare.sh

--- newSysPrepare.sh
+++ newSysPrepare.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 #懒的一个一个下载了，直接拉一群自动执行
 
+apt update
+apt install htop iftop dnsutils traceroute mtr curl python python-pip -y && apt-get clean
+
 SCR_LIST="bbr.sh,jdkenv1.8.sh,lnmp.sh,mkswap.sh,setHttps.sh,v2ray.sh"
 DOWNADDR="https://github.com/sunflyer/EasyUbuntu/raw/master/"
 
