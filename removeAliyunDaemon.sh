058fdaf06398e96508a01036d9d63abe3ffe54c2
Wilson Chen <sunflyer@users.noreply.github.com>
2019-02-25 21:30:11 +0800

    Update removeAliyunDaemon.sh

--- removeAliyunDaemon.sh
+++ removeAliyunDaemon.sh
@@ -6,4 +6,4 @@ curl -sSL http://update.aegis.aliyun.com/download/quartz_uninstall.sh | sudo bas
 sudo rm -rf /usr/local/aegis
 sudo rm /usr/sbin/aliyun-service*
 sudo rm /lib/systemd/system/aliyun.service
-
+killall -9 aliyun-service
